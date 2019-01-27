08584a6be370c5c31cacd9e69da9c8ceed792ca3
MarkMcconachie <Markm1206@gmail.com>
2019-01-27 01:33:49 -0600

    added button

--- scheduler/schedulox/models.py
+++ scheduler/schedulox/models.py
@@ -9,7 +9,8 @@ CATEGORIES = (
 
 # Create your models here.
 class Course(models.Model):
-	course_name = models.CharField(max_length=200)
+	subject_name = models.CharField(max_length=200)
+	course_number = models.CharField(max_length=200)
 	
 	def __str__(self):
 		return self.course_name
